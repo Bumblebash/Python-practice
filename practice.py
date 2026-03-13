baf7aa9647ff7ac6ee7179787dc7029550c9e62f
Bumblebash <ssimbwabashir777@gmail.com>
2026-03-13 12:16:06 +0200

    Scripts to connect and push data to SQL server Database

---
+++ practice.py
@@ -0,0 +1,28 @@
+count = 0
+numbers = [1,2,3,4,5,678,98,100]
+for num in numbers:
+    count = count +1 
+print ("count:", count)
+
+# TOTAL
+TOTAL = 0
+numbers = [1,2,4,56,7,89,90]
+for num in numbers:
+    total = total + num
+print ('total:', total)
+
+
+#Condition for num > 6
+total = 0
+numbers = [1,2,3,44,667.87,90, 102, 167, 189]
+for num in numbers:
+    if num > 100:
+        total = total + num
+print('total:', total)
+
+
+#prompt a User for an Input
+total = 0
+count = 0
+num = 0
+
